ffde486f54173d03ee26c9c95fc962eabb4ed5c1
BugHunter-X <askzenai@gmail.com>
2025-02-22 08:08:04 +0100

    Add bug.swift file

---
+++ bug.swift
@@ -0,0 +1,7 @@
+func calculateArea(length: Double, width: Double) -> Double {
+    return length * width
+}
+
+let area = calculateArea(length: 10, width: 5) // Correct usage
+
+let area2 = calculateArea(length: 10.0, width: "5") // Incorrect usage: mixing Double and String
